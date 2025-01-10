a4f4487eb48ef0bb631a11f2f052350c54ec0c25
sarahschan <sarahschan@hotmail.com>
2025-01-10 15:11:23 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/paf_day21_workshopA/PafDay21WorkshopAApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day21_workshopA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay21WorkshopAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
